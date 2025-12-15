8cb950bba99168598aeefe43cc90cc50d498c18a
MayangAsura <mayang.asura123@gmail.com>
2025-12-15 12:33:51 +0700

    remove log file

--- index.js
+++ index.js
@@ -65,6 +65,6 @@ app.use((err, req, res, next) => {
 
 
 app.listen(8080, () => {
-    logger.info(`Server is running on port 8080`),
-    console.log(`Server is running on port 8080`)
+    console.log(`Server is running on port 8080`),
+    logger.info(`Server is running on port 8080`)
 })
