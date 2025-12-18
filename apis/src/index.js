e2ea14814f51f72d4b3231ccb2232364fcc56aa2
MayangAsura <mayang.asura123@gmail.com>
2025-12-18 10:16:01 +0700

    updated index

--- apis/src/index.js
+++ apis/src/index.js
@@ -40,19 +40,20 @@ mongoose.connection.on('connected', () =>{
 //     // res.send('I am on middleware')
 // })
 
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    next();
-});
+// app.use(function (req, res, next) {
+//     //Enabling CORS
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+//     next();
+// });
 
-app.get('/',  (req, res) => {
+app.get('/test',  (req, res) => {
     console.log('Hello there')
     res.send('AirBooking API')
 })
